9d17c3fb571c1c7a673368e12fa34f808bfc4cf1
Dominik Roje <roje.dominik@gmail.com>
2024-12-18 18:15:00 +0100

    praktični zadatak 2 prvi dio

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from main.views import ProjectList, TaskList, CategoryList, ReminderList, TimelogList
 from django.contrib.auth import views as auth_views
 
 
@@ -7,5 +8,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    path('project/', ProjectList.as_view()),
+    path('task/', TaskList.as_view()),
+    path('category/', CategoryList.as_view()),
+    path('reminder/', ReminderList.as_view()),
+    path('timelog/', TimelogList.as_view())
+
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,12 +1,74 @@
 from django.shortcuts import render, redirect
 from . import urls
+from django.db.models import Q
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from django.views.generic import ListView
+from main.models import Project, Task, Category, TimeLog, Reminder
+
+class ProjectList(ListView):
+    model = Project
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')  
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset
+
+class TaskList(ListView):
+    model = Task
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset
+
+class CategoryList(ListView):
+    model = Category
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')  
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset
+
+class TimelogList(ListView):
+    model = TimeLog
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')  
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset
+
+class ReminderList(ListView):
+    model = Reminder
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')  
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset
+
 def index(request):
     return render(request, 'main/index.html')
-
 
 
 def register(request):
@@ -28,3 +90,4 @@ def register(request):
     context = {'form': form}
 
     return render(request, 'registration/register.html', context)
+
